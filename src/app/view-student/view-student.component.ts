66390ae1c4dabfd327e07e4d44bdb038c45e0196
Bachi007 <34607489+Bachi007@users.noreply.github.com>
2022-05-27 23:00:05 +0530

    admin changes

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MatTableDataSource} from '@angular/material/table';
+import { DataService } from '../service/data.service';
 import { QretriveService } from '../service/qretrive.service';
 @Component({
   selector: 'app-view-student',
@@ -8,10 +9,15 @@ import { QretriveService } from '../service/qretrive.service';
   styleUrls: ['./view-student.component.css']
 })
 export class ViewStudentComponent implements OnInit {
+  userScore: any=[];
+  username: any;
 
 
-  constructor(private qrtr:QretriveService) { }
+  constructor(private qrtr:QretriveService,private dataservice:DataService) { }
   user:any=[];
+  userdetails:any={
+  
+  }
   ngOnInit() {
     var rqtn={
       "role":"user",
@@ -20,8 +26,14 @@ export class ViewStudentComponent implements OnInit {
     this.qrtr.uretrive(rqtn).subscribe(res=>{
      // this.qtn=res;
       console.log(res);
-      this.user=res
+      this.user=res;
+      for(let enduser of this.user){
+    this.dataservice.getuserdata(enduser['username']).subscribe((res)=>{
+      this.userScore=res['Scores']
+      this.username=res['username']
+      console.log(this.userScore )
         })
+      }
+  });
   }
-
 }
